e54228f3cdea034e36087b5a4c96c1608a2249e1
Suganthi Jayaseelan <suganthi1483@gmail.com>
2018-10-09 16:33:00 +0000

    Game plan

---
+++ gamePlanning.rb
@@ -0,0 +1,17 @@
+# Players class
+
+# Player number and number of lives
+# var Player numbers
+# var lives
+
+# Game class 
+# Players as parameters
+# changes managed here - lives left
+# user input
+# Turn and Question
+# var Turn number
+# method: (lives left for the player )
+# method: figures out who's turn it is - set current player and switch ever time
+# method: game loop (where each instance of the loop is the other players turn) - diff to above??
+# method: creates a math question based on equation - could be a separste function - 
+# how to kmow whem game is over?? - needs lives left
